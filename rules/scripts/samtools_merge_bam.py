9ee11f252f4b504ebc73f834b523a81af6b883b9
Gianmauro Cuccuru <gmauro@crs4.it>
2017-10-11 17:28:06 +0200

    typo

--- rules/scripts/samtools_merge_bam.py
+++ rules/scripts/samtools_merge_bam.py
@@ -1,7 +1,7 @@
 from subprocess import run
 
 if len(snakemake.input) > 1:
-    subprocess.run(['samtools', 'merge', snakemake.output, snakemake.input])
+    run(['samtools', 'merge', snakemake.output, snakemake.input])
 else:
-     subprocess.run(['cp', snakemake.input, snakemake.output])
-     subprocess.run(['touch', '-h', snakemake.output])
+     run(['cp', snakemake.input, snakemake.output])
+     run(['touch', '-h', snakemake.output])
