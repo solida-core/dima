f6167c30459a538675fdf8ccf23c40daffc2ce81
Gianmauro Cuccuru <gmauro@crs4.it>
2017-10-19 10:44:08 +0200

    arguments fix

--- rules/scripts/samtools_merge_bam.py
+++ rules/scripts/samtools_merge_bam.py
@@ -1,7 +1,7 @@
 from subprocess import run
 
 if len(snakemake.input) > 1:
-    run(['samtools', 'merge', snakemake.output, snakemake.input])
+    run(['samtools', 'merge', snakemake.output[0], snakemake.input[0], snakemake.input[1]])
 else:
-     run(['cp', snakemake.input, snakemake.output])
-     run(['touch', '-h', snakemake.output])
+     run(['cp', snakemake.input[0], snakemake.output[0]])
+     run(['touch', '-h', snakemake.output[0]])
