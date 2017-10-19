c9c74918003d7257a663c728f0e8b5421c254746
Gianmauro Cuccuru <gmauro@crs4.it>
2017-10-19 16:45:42 +0200

    now samtools merge script can handle multiple inputs

--- rules/scripts/samtools_merge_bam.py
+++ rules/scripts/samtools_merge_bam.py
@@ -1,7 +1,10 @@
 from subprocess import run
 
 if len(snakemake.input) > 1:
-    run(['samtools', 'merge', snakemake.output[0], snakemake.input[0], snakemake.input[1]])
+    cmd = ['samtools', 'merge', snakemake.output[0]]
+    for i in snakemake.input:
+        cmd.append(i)
+    run(cmd)
 else:
      run(['cp', snakemake.input[0], snakemake.output[0]])
      run(['touch', '-h', snakemake.output[0]])
